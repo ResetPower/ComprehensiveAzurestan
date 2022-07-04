3c715f7b2a5d5356c1f355e6943166945626e1c9
ResetPower <resetpower20@gmail.com>
2022-07-04 15:29:00 +0800

    milestone: hello, world!

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/central")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot:1.18.2-R0.1-SNAPSHOT")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/space/azurestan/Comprehensive.kt
@@ -0,0 +1,13 @@
+package space.azurestan
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Comprehensive : JavaPlugin() {
+    override fun onEnable() {
+        println("Comprehensive Azurestan Plugin Enabled!")
+    }
+
+    override fun onDisable() {
+        println("Comprehensive Azurestan Plugin Disabled!")
+    }
+}
